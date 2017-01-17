0bcae44f5d0a86269374f4704e13eed10681ddc9
George Miller <git@duffleman.co.uk>
2017-01-17 19:23:11 +0000

    Implements both NI formats

--- src/Formats/ni_1903.php
+++ src/Formats/ni_1903.php
@@ -3,5 +3,43 @@
 namespace Duffleman\VRM\Formats;
 
 class ni_1903 implements FormatInterface {
-	public function parse(string $vrm) {}
+	const validFrom = 1903;
+	const regexOriginal = '/^([A-Z]{2})(\d{1,4})$/';
+	const regexReversed = '/^(\d{1,4})([A-Z]{2})$/';
+	const areas = [
+		'AZ', 'BZ', 'CZ', 'DZ', 'EZ', 'FZ', 'GZ', 'HZ', 'IA', 'IB', 'IG', 'IJ',
+		'IL', 'IW', 'JI', 'JZ', 'KZ', 'LZ', 'MZ', 'NZ', 'OI', 'OZ', 'PZ', 'RZ',
+		'SZ', 'TZ', 'UI', 'UZ', 'VZ', 'WZ', 'XI', 'XZ', 'YZ',
+	];
+
+	public function parse(string $vrm) {
+		$original = preg_match(self::regexOriginal, $vrm, $originalOut);
+		$reversed = preg_match(self::regexReversed, $vrm, $reversedOut);
+		$match = $original || $reversed;
+
+		if (!$match) {
+			return null;
+		}
+
+		$isOriginal = !!$original;
+		$area = $isOriginal ? $originalOut[1] : $reversedOut[2];
+		$seq = intval($isOriginal ? $originalOut[2] : $reversedOut[1], 10);
+		$matchOut = $isOriginal ? $originalOut : $reversedOut;
+
+		if (!in_array($area, self::areas)) {
+			return null;
+		}
+
+		array_shift($matchOut);
+
+		return [
+			'prettyVrm' => implode($matchOut, ' '),
+
+			'_extra' => [
+				'reversed' => !$isOriginal,
+				'area' => $area,
+				'sequence' => $seq,
+			],
+		];
+	}
 }
--- src/Formats/ni_1966.php
+++ src/Formats/ni_1966.php
@@ -3,5 +3,39 @@
 namespace Duffleman\VRM\Formats;
 
 class ni_1966 implements FormatInterface {
-	public function parse(string $vrm) {}
+	const validFrom = 1966;
+	const regex = '/^([A-Z]{3})(\d{1,4})$/';
+	const areas = [
+		'AZ', 'BZ', 'CZ', 'DZ', 'EZ', 'FZ', 'GZ', 'HZ', 'IA', 'IB', 'IG', 'IJ',
+		'IL', 'IW', 'JI', 'JZ', 'KZ', 'LZ', 'MZ', 'NZ', 'OI', 'OZ', 'PZ', 'RZ',
+		'SZ', 'TZ', 'UI', 'UZ', 'VZ', 'WZ', 'XI', 'XZ', 'YZ',
+	];
+
+	public function parse(string $vrm) {
+		$match = preg_match(self::regex, $vrm, $matchOut);
+
+		if ($match !== 1) {
+			return null;
+		}
+
+		$serial = substr($matchOut[1], 0, 1);
+		$area = substr($matchOut[1], 1, 2);
+		$seq = intval($matchOut[2], 10);
+
+		if (!in_array($area, self::areas)) {
+			return null;
+		}
+
+		array_shift($matchOut);
+
+		return [
+			'prettyVrm' => implode($matchOut, ' '),
+
+			'_extra' => [
+				'serialLetter' => $serial,
+				'area' => $area,
+				'sequence' => $seq,
+			],
+		];
+	}
 }
